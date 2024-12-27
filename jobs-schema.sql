9271dc58da1f58c1f268a9f4d8dd01ef4343590f
ya2min3 <yasminebelbahri@gmail.com>
2024-12-27 01:30:48 +0100

    code to create jobs table

---
+++ jobs-schema.sql
@@ -0,0 +1,17 @@
+create table jobs (
+    job_ID numeric (10,0),
+    designation text,
+    company_id numeric(5,1),
+    name text,
+    work_type text,
+    involvement text,
+    employees_count numeric(5,0),
+    total_applicants numeric(5,0),
+    linkedin_followers numeric,
+    job_details text,
+    details_id numeric(5,0),
+    industry text,
+    level text,
+    City text,
+    State text
+);
