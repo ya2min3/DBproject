0e7ebd3ab06499fe385a4f91a6e77579998773a9
Melissahachemi <158464595+Melissahachemi@users.noreply.github.com>
2024-12-28 00:40:25 +0100

    added system signup/login

---
+++ user-schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(320) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    photo_path TEXT,
+    cv_path TEXT
+);
